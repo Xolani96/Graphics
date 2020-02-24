ad28b0e81966fa81cb52da1e449765ec1738e4b9
Xolani Nhlapo <Nhlapho7@gmail.com>
2020-02-24 09:08:15 +0100

    Graphics project start

---
+++ Lesson1/Client/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Graphics.Client
+{
+    internal class Program: 
+        System.Windows.Application , IDisposable
+       
+    {
+        #region Constructor
+        public Program()
+        {
+            Startup += (sender, args) => Ctor(); // event provided by the windows app class
+            Exit += (sender, args) => Dispose();
+        }
+        private void Ctor()
+        {
+            
+        } 
+        public void Dispose()
+        {
+
+        }
+        #endregion
+    }
+}
---
+++ Lesson1/EntryPoint.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Graphics
+{
+    internal class EntryPoint
+    {
+        static void Main(string[] args) => new Client.Program().Run(); 
+    }
+}
